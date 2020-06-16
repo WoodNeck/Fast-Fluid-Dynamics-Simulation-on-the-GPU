14f46a4a34ebf6dc43600cf7310821afa15799e6
WoodNeck <saiyu915@naver.com>
2020-06-16 12:58:25 +0900

    Update

--- src/shaders/color-restriction.fs
+++ src/shaders/color-restriction.fs
@@ -9,15 +9,17 @@ in vec2 vUv;
 out vec4 fragColor;
 
 vec2 getPaletteUV(vec4 col) {
-	vec3 mappedColors = floor(col.rgb * 15.);
-	float mappedI = mappedColors.r * 256. + mappedColors.g * 16. + mappedColors.b;
-	return vec2(mod(mappedI, 64.), mappedI / 64.) / 64.;
-	// return vec2(col.r, col.r);
+	// vec3 mappedColors = floor(col.rgb * 15.);
+	// float mappedI = mappedColors.r * 256. + mappedColors.g * 16. + mappedColors.b;
+	// return vec2(mod(mappedI, 64.), mappedI / 64.) / 64.;
+	return vec2(col.r, col.r);
 }
 
 void main() {
 	vec4 albedo = texture(uTex, vUv);
   vec2 pUV = getPaletteUV(albedo);
+	pUV = floor(pUV * 5.) / 5.;
+
 	vec4 restricted = texture(uPalette, pUV);
 	fragColor = restricted;
 }
