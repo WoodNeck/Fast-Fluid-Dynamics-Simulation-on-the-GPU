98961a39483502c6a5c97435989360bfa20a50e9
WoodNeck <saiyu915@naver.com>
2020-06-16 12:58:39 +0900

    Updates

--- dist/app.js
+++ dist/app.js
@@ -50376,7 +50376,7 @@ version: 1.0.0
 	    return GradientSubtractPass;
 	}(Pass));
 
-	var colorRestrictionFS = "#version 300 es\nprecision highp float;precision highp sampler2D;\n#define GLSLIFY 1\nuniform sampler2D uTex;uniform sampler2D uPalette;in vec2 vUv;out vec4 fragColor;vec2 getPaletteUV(vec4 col){vec3 mappedColors=floor(col.rgb*15.);float mappedI=mappedColors.r*256.+mappedColors.g*16.+mappedColors.b;return vec2(mod(mappedI,64.),mappedI/64.)/64.;}void main(){vec4 albedo=texture(uTex,vUv);vec2 pUV=getPaletteUV(albedo);vec4 restricted=texture(uPalette,pUV);fragColor=restricted;}"; // eslint-disable-line
+	var colorRestrictionFS = "#version 300 es\nprecision highp float;precision highp sampler2D;\n#define GLSLIFY 1\nuniform sampler2D uTex;uniform sampler2D uPalette;in vec2 vUv;out vec4 fragColor;vec2 getPaletteUV(vec4 col){return vec2(col.r,col.r);}void main(){vec4 albedo=texture(uTex,vUv);vec2 pUV=getPaletteUV(albedo);pUV=floor(pUV*5.)/5.;vec4 restricted=texture(uPalette,pUV);fragColor=restricted;}"; // eslint-disable-line
 
 	function range(end) {
 	    var n, i;
