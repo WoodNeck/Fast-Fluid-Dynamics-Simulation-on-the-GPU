dd2ccedb18bef099655c68f9307b2707ec5d577b
WoodNeck <saiyu915@naver.com>
2020-06-16 13:50:29 +0900

    Updates

--- dist/app.js
+++ dist/app.js
@@ -50497,6 +50497,171 @@ version: 1.0.0
 	}());
 
 	// https://lospec.com/palette-list/sweetie-16
+	var SWEETIE16 = {
+	    name: "sweetie-16",
+	    outline: "#1a1c2c",
+	    colors: [
+	        "#1a1c2c",
+	        "#5d275d",
+	        "#b13e53",
+	        "#ef7d57",
+	        "#ffcd75",
+	        "#a7f070",
+	        "#38b764",
+	        "#257179",
+	        "#29366f",
+	        "#3b5dc9",
+	        "#41a6f6",
+	        "#73eff7",
+	        "#f4f4f4",
+	        "#94b0c2",
+	        "#566c86",
+	        "#333c57",
+	    ],
+	};
+	// https://lospec.com/palette-list/endesga-16
+	var ENDESGA16 = {
+	    name: "endesga-16",
+	    outline: "#0484d1",
+	    colors: [
+	        "#e4a672",
+	        "#b86f50",
+	        "#743f39",
+	        "#3f2832",
+	        "#9e2835",
+	        "#e53b44",
+	        "#fb922b",
+	        "#ffe762",
+	        "#63c64d",
+	        "#327345",
+	        "#193d3f",
+	        "#4f6781",
+	        "#afbfd2",
+	        "#ffffff",
+	        "#2ce8f4",
+	        "#0484d1",
+	    ],
+	};
+	// https://lospec.com/palette-list/ice-cream-gb
+	var ICE_CREAM_GB = {
+	    name: "ice-cream-gb",
+	    outline: "#3e1f2c",
+	    colors: [
+	        "#7c3f58",
+	        "#eb6b6f",
+	        "#f9a875",
+	        "#fff6d3",
+	    ],
+	};
+	// https://lospec.com/palette-list/indecision
+	var INDECISION = {
+	    name: "indecision",
+	    outline: "#2c1b2e",
+	    colors: [
+	        "#fff4e0",
+	        "#8fcccb",
+	        "#449489",
+	        "#285763",
+	        "#2f2b5c",
+	        "#4b3b9c",
+	        "#457cd6",
+	        "#f2b63d",
+	        "#d46e33",
+	        "#e34262",
+	        "#94353d",
+	        "#57253b",
+	        "#9c656c",
+	        "#d1b48c",
+	        "#b4ba47",
+	        "#6d8c32",
+	        "#2c1b2e",
+	    ],
+	};
+	// https://lospec.com/palette-list/island-joy-16
+	var ISLAND_JOY_16 = {
+	    name: "island-joy-16",
+	    outline: "#393457",
+	    colors: [
+	        "#ffffff",
+	        "#6df7c1",
+	        "#11adc1",
+	        "#606c81",
+	        "#393457",
+	        "#1e8875",
+	        "#5bb361",
+	        "#a1e55a",
+	        "#f7e476",
+	        "#f99252",
+	        "#cb4d68",
+	        "#6a3771",
+	        "#c92464",
+	        "#f48cb6",
+	        "#f7b69e",
+	        "#9b9c82",
+	    ],
+	};
+	// https://lospec.com/palette-list/nintendo-entertainment-system
+	var NES = {
+	    name: "nes",
+	    outline: "#000000",
+	    colors: [
+	        "#000000",
+	        "#fcfcfc",
+	        "#f8f8f8",
+	        "#bcbcbc",
+	        "#7c7c7c",
+	        "#a4e4fc",
+	        "#3cbcfc",
+	        "#0078f8",
+	        "#0000fc",
+	        "#b8b8f8",
+	        "#6888fc",
+	        "#0058f8",
+	        "#0000bc",
+	        "#d8b8f8",
+	        "#9878f8",
+	        "#6844fc",
+	        "#4428bc",
+	        "#f8b8f8",
+	        "#f878f8",
+	        "#d800cc",
+	        "#940084",
+	        "#f8a4c0",
+	        "#f85898",
+	        "#e40058",
+	        "#a80020",
+	        "#f0d0b0",
+	        "#f87858",
+	        "#f83800",
+	        "#a81000",
+	        "#fce0a8",
+	        "#fca044",
+	        "#e45c10",
+	        "#881400",
+	        "#f8d878",
+	        "#f8b800",
+	        "#ac7c00",
+	        "#503000",
+	        "#d8f878",
+	        "#b8f818",
+	        "#00b800",
+	        "#007800",
+	        "#b8f8b8",
+	        "#58d854",
+	        "#00a800",
+	        "#006800",
+	        "#b8f8d8",
+	        "#58f898",
+	        "#00a844",
+	        "#005800",
+	        "#00fcfc",
+	        "#00e8d8",
+	        "#008888",
+	        "#004058",
+	        "#f8d8f8",
+	        "#787878",
+	    ],
+	};
 	// https://lospec.com/palette-list/nintendo-super-gameboy
 	var SUPER_GAMEBOY = {
 	    name: "nintendo-super-gameboy",
@@ -50507,6 +50672,29 @@ version: 1.0.0
 	        "#d68e49",
 	        "#f7e7c6",
 	    ],
+	};
+	var STONE_BRICK = {
+	    name: "stone-brick",
+	    outline: "#393121",
+	    colors: [
+	        "#7b8452",
+	        "#6b6b42",
+	        "#9da673",
+	        "#634a29",
+	        "#423931",
+	    ],
+	};
+
+	var COLORS = {
+		__proto__: null,
+		SWEETIE16: SWEETIE16,
+		ENDESGA16: ENDESGA16,
+		ICE_CREAM_GB: ICE_CREAM_GB,
+		INDECISION: INDECISION,
+		ISLAND_JOY_16: ISLAND_JOY_16,
+		NES: NES,
+		SUPER_GAMEBOY: SUPER_GAMEBOY,
+		STONE_BRICK: STONE_BRICK
 	};
 
 	var ColorRestrictionPass = /** @class */ (function (_super) {
@@ -50753,6 +50941,7 @@ version: 1.0.0
 	        colorResPass.on(AFTER_RENDER, function () {
 	            pixelated.swap();
 	        });
+	        this._colorResPass = colorResPass;
 	        var copyPass = new CopyPass();
 	        copyPass.on(BEFORE_RENDER, function () {
 	            copyPass.plane.updateUniforms({
@@ -50771,13 +50960,29 @@ version: 1.0.0
 	        renderer.addPass(copyPass);
 	    };
 	    App.prototype._setupControls = function () {
+	        var _this = this;
 	        // @ts-ignore
 	        var gui = new dat.GUI();
+	        var color = { palette: "SUPER_GAMEBOY" };
+	        var palettes = [
+	            "SWEETIE16",
+	            "ENDESGA16",
+	            "ICE_CREAM_GB",
+	            "INDECISION",
+	            "ISLAND_JOY_16",
+	            "NES",
+	            "SUPER_GAMEBOY"
+	        ];
 	        gui.add(this, "curlStrength", 0, 100);
 	        gui.add(this, "radius", -5, 0);
 	        gui.add(this, "densityDissipation", 0, 1);
 	        gui.add(this, "velocityDissipation", 0, 1);
 	        gui.add(this, "pressureDissipation", 0, 1);
+	        gui.add(color, "palette", palettes).onChange(function () {
+	            _this._colorResPass.plane.updateUniforms({
+	                uPalette: PaletteTexture.get(COLORS[color.palette]),
+	            });
+	        });
 	    };
 	    return App;
 	}());
